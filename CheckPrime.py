2277012d7c7f4e8555629ed246686aa73e4ca15d
dvchary <varaprasad.achary@gmail.com>
2020-10-15 15:36:53 +0530

    removed ; wherever is there

--- CheckPrime.py
+++ CheckPrime.py
@@ -1,20 +1,20 @@
 # Program to check if a number is prime or not
 
-num = int(input("Please Enter a Number between 1 to 50: "));
+num = int(input("Please Enter a Number between 1 to 50: "))
 
 # Prime Numbers are greater than 1
 
 if num > 50:
-	    num = int(input("Please Enter a Number between 1 to 50: "));
+	    num = int(input("Please Enter a Number between 1 to 50: "))
 		
 if num > 1:
 	#Check for factors
 	for i in range(2,num):
 		if (num % i) == 0:
-			print(num, " is not a prime number");
-			print(i, " times ", num//i, " is ", num);
+			print(num, " is not a prime number")
+			print(i, " times ", num//i, " is ", num)
 			break
 	else:
-		print(num, "is a prime number");
+		print(num, "is a prime number")
 else:
-	print(num, "is not a prime number");
+	print(num, "is not a prime number")
