284faa728efc7bd64374ed044d9cf1f7e6dd5473
dvchary <varaprasad.achary@gmail.com>
2020-10-15 15:34:53 +0530

    removed ; wherever are there in the code

--- Top3.py
+++ Top3.py
@@ -8,15 +8,15 @@
 
 # uncomment following lines to take three numbers from user
 
-num1 = float(input("Enter the 1st Number "));
-num2 = float(input("Enter the 2nd Number "));
-num3 = float(input("Enter the 3rd Number "));
+num1 = float(input("Enter the 1st Number: "))
+num2 = float(input("Enter the 2nd Number: "))
+num3 = float(input("Enter the 3rd Number: "))
 
 if (num1 >= num2) and (num1 >= num3):
-	largest = num1;
+	largest = num1
 elif (num2 >= num1) and (num2 >= num3):
-	largest = num2;
+	largest = num2
 else:
-	largest = num3;
-print("The Largest Number is ", largest);
+	largest = num3
+print("The Largest Number is ", largest)
 	
